37369a5d3eb90c02cfdeff79a2c2040733d41817
Giselda  Arevalo <96034020+giselarevalo@users.noreply.github.com>
2023-08-27 22:56:16 -0300

    create index and register structure

---
+++ Controllers/usersController.js
@@ -0,0 +1,13 @@
+module.exports = {
+    register : (req,res)=>{
+        res.render('register')
+    },
+    login : (req,res)=>{
+        
+    },
+    profile : (req,res)=>{
+        
+    }
+
+    
+}
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+
+const {register} = require('../Controllers/usersController');
+/* /users */
+router.get('/register',register ); 
 
 module.exports = router;
+
+
+
+
+
